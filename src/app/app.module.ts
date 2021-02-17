4da5ffe72bff0492bcf92c3fffe80b8fd9c9ca49
Igor Muniz <igor.muniz@championx.com>
2021-02-17 19:44:39 -0300

    Including new components, styles and services;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navigation/menu/menu.component';
 import { HomeComponent } from './navigation/home/home.component';
 import { FooterComponent } from './navigation/footer/footer.component';
+import { AboutComponent } from './institutional/about/about.component';
+import { ContactComponent } from './institutional/contact/contact.component';
+import { rootRouterConfig } from './app.routes';
+import { APP_BASE_HREF } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from 'src/products/products.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false })]
   ],
-  providers: [],
+  providers: [
+    ProductService,
+    { provide: APP_BASE_HREF, useValue: '/' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/products/product.ts
@@ -0,0 +1,8 @@
+export class Product {
+    id: string;
+    name: string;
+    price: number;
+    onSale: boolean;
+    onSalePrice: number;
+    imageUrl: string;
+}
